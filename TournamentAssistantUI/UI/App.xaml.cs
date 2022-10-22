28740abc826b860b7446833d59b502cfdcd8215a
nanikit <nanikit4@gmail.com>
2022-10-22 23:05:40 +0900

    feat: add assistant ui error logger

--- TournamentAssistantUI/UI/App.xaml.cs
+++ TournamentAssistantUI/UI/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using TournamentAssistantUI.Misc;
 using System.Windows;
+using System;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace TournamentAssistantUI.UI
 {
@@ -8,6 +11,50 @@ namespace TournamentAssistantUI.UI
     /// </summary>
     public partial class App : Application
     {
+        private StreamWriter _logWriter;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            _logWriter = File.AppendText($"{DateTime.Now:yyMMdd-HHmmss.log}");
+            SetupExceptionHandling();
+        }
+
+        private void SetupExceptionHandling()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+                LogUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException");
+                e.Handled = true;
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                LogUnhandledException(e.Exception, "TaskScheduler.UnobservedTaskException");
+                e.SetObserved();
+            };
+        }
+
+        private void LogUnhandledException(Exception exception, string source)
+        {
+            try
+            {
+                string message = $"Unhandled exception ({source}) {exception}";
+                System.Reflection.AssemblyName assemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName();
+                message = string.Format("Unhandled exception in {0} v{1}", assemblyName.Name, assemblyName.Version);
+                _logWriter.WriteLine(message);
+            }
+            catch (Exception ex)
+            {
+                string message = $"Exception of unhandled ({source}) {ex}";
+                _logWriter.WriteLine(message);
+            }
+        }
+
         private void Application_Exit(object sender, ExitEventArgs e)
         {
             MouseHook.StopHook();
